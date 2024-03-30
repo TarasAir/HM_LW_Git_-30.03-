7766a6dd87a063185a6f51f9168037e7584a8950
Taras <kornienkotaras2@gmail.com>
2024-03-30 20:44:42 +0200

    method DivNumbers

--- HM_LW_Git_(30.03)/HM_LW_Git_(30.03).cpp
+++ HM_LW_Git_(30.03)/HM_LW_Git_(30.03).cpp
@@ -13,6 +13,12 @@ int DiffNumbers(int number1, int number2)
     return result;
 }
 
+int DivNumbers(int number1, int number2)
+{
+    int result = number1 / number2;
+    return result;
+}
+
 int main()
 {
     
